2fd4c9274e16bbcfc67449f0c1b6abec9fad1a58
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-09 18:27:27 +0100

    Delete src directory

--- src/todo/components/TodoItem.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./TodoItem.css";
-
-const TodoItem = (props) => {
-  return <div>TodoItem</div>;
-};
-
-export default TodoItem;
--- src/todo/components/TodoList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./TodoList.css";
-
-const TodoList = (props) => {
-  return <div>TodoList</div>;
-};
-
-export default TodoList;
