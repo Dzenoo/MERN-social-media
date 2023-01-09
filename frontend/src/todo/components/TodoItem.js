ef8aae275a759ab1eaa34c8fa557a95c1e1ecb51
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-09 18:25:45 +0100

    Add files via upload

---
+++ frontend/src/todo/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./TodoItem.css";
+
+const TodoItem = (props) => {
+  return <div>TodoItem</div>;
+};
+
+export default TodoItem;
---
+++ frontend/src/todo/components/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./TodoList.css";
+
+const TodoList = (props) => {
+  return <div>TodoList</div>;
+};
+
+export default TodoList;
