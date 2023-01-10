f4eea8f4a7ecfb827a3be727c916b1b73b77d4dc
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-10 14:53:38 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import { AuthContext } from "./shared/context/auth-context";
 import { Navigate, Route, Routes } from "react-router-dom";
 
@@ -7,9 +7,20 @@ import Todos from "./todo/pages/TodoPage";
 import CreateTodo from "./todo/pages/CreateTodo";
 import UpdateTodo from "./todo/pages/UpdateTodo";
 import Profile from "./user/pages/Profile";
+import ProfileSettings from "./user/components/ProfileSettings";
 import Auth from "./user/pages/Auth";
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setisLoggedIn(true);
+  });
+
+  const logout = useCallback(() => {
+    setisLoggedIn(false);
+  });
+
   // const authCtx = useContext(AuthContext);
 
   // let routes;
@@ -35,15 +46,20 @@ function App() {
 
   return (
     <>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<Todos />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/new" element={<CreateTodo />} />
-        <Route path="/profile/:todoId" element={<UpdateTodo />} />
-        <Route path="/register" element={<Auth />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      >
+        <MainNavigation />
+        <Routes>
+          <Route path="/" element={<Todos />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/settings" element={<ProfileSettings />} />
+          <Route path="/profile/new" element={<CreateTodo />} />
+          <Route path="/profile/:todoId" element={<UpdateTodo />} />
+          <Route path="/register" element={<Auth />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AuthContext.Provider>
     </>
   );
 }
