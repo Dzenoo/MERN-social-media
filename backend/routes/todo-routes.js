476d288993f7174658931c51c4f7334f02d4b35c
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-12 23:08:53 +0100

    Add files via upload

--- backend/routes/todo-routes.js
+++ backend/routes/todo-routes.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const { check } = require("express-validator");
+const checkAuth = require("../middlewares/auth");
+const fileUpload = require("../middlewares/file-upload");
 
 const todoContollers = require("../controllers/todo-controllers");
 
 const router = express.Router();
 
+router.get("/", todoContollers.getTodos);
+
 router.get("/user/:uid", todoContollers.getTodoByUserId);
+
+router.use(checkAuth);
 
 router.post(
   "/new",
+  fileUpload.single("image"),
   [
     check("title").not().isEmpty(),
     check("description").not().isEmpty(),
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -2,11 +2,18 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const userContollers = require("../controllers/user-controllers");
+const fileUpload = require("../middlewares/file-upload");
 
 const router = express.Router();
 
+router.get("/", userContollers.getUsers);
+
+router.get("/:id", userContollers.getUserProfile);
+
 router.post(
   "/signup",
+  fileUpload.single("image"),
+
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
