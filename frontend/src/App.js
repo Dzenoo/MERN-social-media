63e56110ab770e710927d655deb3a227619f0bc8
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-12 23:09:29 +0100

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React, { useCallback, useContext, useState } from "react";
+import React from "react";
 import { AuthContext } from "./shared/context/auth-context";
+import { useAuth } from "./shared/hooks/auth-hook";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
@@ -10,27 +11,25 @@ import Profile from "./user/pages/Profile";
 import Auth from "./user/pages/Auth";
 
 function App() {
-  const [isLoggedIn, setisLoggedIn] = useState(false);
-
-  const login = useCallback(() => {
-    setisLoggedIn(true);
-  });
-
-  const logout = useCallback(() => {
-    setisLoggedIn(false);
-  });
+  const { token, login, logout, userId } = useAuth();
 
   return (
     <>
       <AuthContext.Provider
-        value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+        value={{
+          isLoggedIn: !!token,
+          token: token,
+          userId: userId,
+          login: login,
+          logout: logout,
+        }}
       >
         <MainNavigation />
         <Routes>
           <Route path="/" element={<Todos />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/new" element={<CreateTodo />} />
-          <Route path="/profile/:todoId" element={<UpdateTodo />} />
+          <Route path="/users/:userId" element={<Profile />} />
+          <Route path="/users/new" element={<CreateTodo />} />
+          <Route path="/users/:userId/:todoId" element={<UpdateTodo />} />
           <Route path="/register" element={<Auth />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
